6b9ea619cf60380b57083ecb84ca1915740d167b
suninoy <sunny_nino@163.com>
2019-05-08 11:35:10 +0800

    增加cli

---
+++ VueCliDemo/src/ToDoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div>
+      <input />
+      <button>提交</button>
+      </div>
+    <ul></ul>
+  </div>
+</template>
+
+<script>
+  export default {
+data:function(){
+    return inputValue;
+}
+  }
+
+</script>
+
+<style>
+
+</style>
